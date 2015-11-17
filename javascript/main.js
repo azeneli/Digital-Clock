be26aaa3bbb363bf3f20e0747ead52fe48641326
azeneli <azeneli46@gmail.com>
2015-11-17 18:10:08 -0500

    Add 0 if minutes < 10

--- javascript/main.js
+++ javascript/main.js
@@ -3,18 +3,21 @@
 (function(){ // protect da scope
 
 	function digitalWatch(){
-		var time = new Date();
-		console.log(time);
-		var hours = time.getHours();
-		console.log(hours,  'hours');
-		var minutes = time.getMinutes();
+		var time = new Date()
+			, hours = time.getHours()
+			, minutes = time.getMinutes()
+			, seconds = time.getSeconds();
+		console.log(time, 'time');
+		console.log(hours, 'hour');
 		console.log(minutes, 'minutes');
-		var seconds = time.getSeconds();
 		console.log(seconds, 'seconds');
 
 		// If the seconds digit is less than ten                    
 		if (seconds < 10) {
     		seconds = "0" + seconds;
+		}
+		else if( minutes < 10){
+			minutes = "0" + minutes;
 		}
 
 		// 12 hours Clock
@@ -24,10 +27,10 @@
 		if (hours > 12) {
 		    hours = hours - 12;
 		    meridiem = "PM";
+		    console.log(hours, "Converted Hour");
 		}
-
-		// 0 AM and 0 PM should read as 12
-		if (hours === 0) {
+		else if (hours === 0) {
+		// 0 AM and 0 PM should read as 12	
 		    hours = 12;    
 		}
 
@@ -41,7 +44,5 @@
 	// This makes our clock 'tick' by repeatedly
 	// running the displayTime function every second.
 	setInterval( digitalWatch, 1000);
-
-
 
 })();
