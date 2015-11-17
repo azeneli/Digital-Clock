85de0c536ea1b6770a66baaa5f5b34a7f1d0575d
azeneli <azeneli46@gmail.com>
2015-11-17 17:49:21 -0500

    Convert to 12 hour format

--- javascript/main.js
+++ javascript/main.js
@@ -12,16 +12,35 @@
 		var seconds = time.getSeconds();
 		console.log(seconds, 'seconds');
 
-		// This gets a "handle" to the clock div in our HTML
-   		var clockDiv = document.getElementsByClassName("clock");
+		// If the seconds digit is less than ten                    
+		if (seconds < 10) {
+    		seconds = "0" + seconds;
+		}
 
-   		clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+		// 12 hours Clock
+		var meridiem = 'AM';
+		// Convert from 24 hour to 12 hour format
+		// and keep track of the meridiem.
+		if (hours > 12) {
+		    hours = hours - 12;
+		    meridiem = "PM";
+		}
 
+		// 0 AM and 0 PM should read as 12
+		if (hours === 0) {
+		    hours = 12;    
+		}
+
+    	var clockDiv = document.getElementById('clock');
+    	clockDiv.innerText = hours + ":" + minutes + ":" + seconds + ' ' + meridiem;
+	    console.log(clockDiv);	
 	}
 
 	digitalWatch();
 
-
+	// This makes our clock 'tick' by repeatedly
+	// running the displayTime function every second.
+	setInterval( digitalWatch, 1000);
 
 
 
