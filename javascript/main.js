110badad10c6ecc07cdee31ad5266732fe9ff624
azeneli <azeneli46@gmail.com>
2015-11-17 17:25:55 -0500

    Func that will display time

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,28 @@
 /* CURRENTLY IN: javascript/main.js */
+
+(function(){ // protect da scope
+
+	function digitalWatch(){
+		var time = new Date();
+		console.log(time);
+		var hours = time.getHours();
+		console.log(hours,  'hours');
+		var minutes = time.getMinutes();
+		console.log(minutes, 'minutes');
+		var seconds = time.getSeconds();
+		console.log(seconds, 'seconds');
+
+		// This gets a "handle" to the clock div in our HTML
+   		var clockDiv = document.getElementsByClassName("clock");
+
+   		clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+
+	}
+
+	digitalWatch();
+
+
+
+
+
+})();
